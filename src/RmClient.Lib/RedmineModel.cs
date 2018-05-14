bcfc8ebfcec70032a7ebb86777acf8a4663a8002
t.masuda <masuda@moonmile.net>
2018-05-14 17:49:10 +0900

    コマンドライン版を追加

---
+++ src/RmClient.Lib/RedmineModel.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Moonmile.Redmine.Model
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Identifier { get; set; }
+        public string Description { get; set; }
+        public int Status { get; set; }
+        public bool IsPublic { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime UpdatedOn { get; set; }
+
+        public Project() { }
+        public Project(XElement it)
+        {
+            this.Id = it.Element("id").ToInt();
+            this.Name = it.Element("name").Value;
+            this.Identifier = it.Element("identifier").Value;
+            this.Description = it.Element("description").Value;
+            this.Status = it.Element("status").ToInt();
+            this.IsPublic = it.Element("is_public").ToBool();
+            this.CreatedOn = it.Element("created_on").ToDatetime();
+            this.UpdatedOn = it.Element("updated_on").ToDatetime();
+        }
+    }
+    public class Issue
+    {
+        public int Id { get; set; }
+        public Project Project { get; set; }
+        public Tracker Tracker { get; set; }
+        public Status Status { get; set; }
+        public Priority Priority { get; set; }
+        public User Author { get; set; }
+        public string Subject { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? DueDate { get; set; }
+        public int DoneRatio { get; set; }
+        public bool IsPrivate { get; set; }
+        public int? EstimatedHours { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime UpdatedOn { get; set; }
+        public DateTime? ClosedOn { get; set; }
+
+        public Issue() {
+            this.Project = new Project();
+            this.Tracker = new Tracker();
+            this.Status = new Status();
+            this.Priority = new Priority();
+            this.Author = new User();
+
+        }
+        public Issue(XElement it)
+        {
+            this.Id = it.Element("id").ToInt();
+            this.Project = new Project() { Id = it.Element("project", "id").ToInt(), Name = it.Element("project", "name").Value };
+            this.Tracker = new Tracker() { Id = it.Element("tracker", "id").ToInt(), Name = it.Element("tracker", "name").Value };
+            this.Status = new Status() { Id = it.Element("status", "id").ToInt(), Name = it.Element("status", "name").Value };
+            this.Priority = new Priority() { Id = it.Element("priority", "id").ToInt(), Name = it.Element("priority", "name").Value };
+            this.Author = new User() { Id = it.Element("author", "id").ToInt(), Name = it.Element("author", "name").Value };
+            this.Subject = it.Element("subject").Value;
+            this.Description = it.Element("description").Value;
+            this.StartDate = it.Element("start_date").ToDatetime();
+            this.DueDate = it.Element("due_date").ToDatetimeOrNull();
+            this.DoneRatio = it.Element("done_ratio").ToInt();
+            this.IsPrivate = it.Element("is_private").ToBool();
+            this.EstimatedHours = it.Element("estimated_hours").ToInt();
+            this.CreatedOn = it.Element("created_on").ToDatetime();
+            this.UpdatedOn = it.Element("updated_on").ToDatetime();
+            this.ClosedOn = it.Element("closed_on").ToDatetimeOrNull();
+        }
+        public string ToXml()
+        {
+            var root = new XElement(XName.Get("issue"));
+            root.Add(new XElement(XName.Get("project_id"), this.Project.Id));
+            root.Add(new XElement(XName.Get("tracker_id"), this.Tracker.Id));
+            root.Add(new XElement(XName.Get("status_id"), this.Status.Id));
+            root.Add(new XElement(XName.Get("priority_id"), this.Priority.Id));
+            root.Add(new XElement(XName.Get("subject"), this.Subject));
+            root.Add(new XElement(XName.Get("description"), this.Description));
+            return root.ToString();
+        }
+    }
+
+    public class Tracker
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Tracker() { }
+        public Tracker(XElement it)
+        {
+            this.Id = it.Element("id").ToInt();
+            this.Name = it.Element("name").Value;
+        }
+    }
+    public class Status
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Status() { }
+        public Status(XElement it)
+        {
+            this.Id = it.Element("id").ToInt();
+            this.Name = it.Element("name").Value;
+        }
+    }
+    public class Priority
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Priority() { }
+        public Priority(XElement it)
+        {
+            this.Id = it.Element("id").ToInt();
+            this.Name = it.Element("name").Value;
+        }
+    }
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public User() { }
+        public User(XElement it)
+        {
+            this.Id = it.Element("id").ToInt();
+            if (it.Element("name") != null)
+            {
+                this.Name = it.Element("name").Value;
+            }
+            else if (it.Element("firstname") != null)
+            {
+                this.Name = it.Element("firstname").Value + " " + it.Element("lastname").Value;
+            }
+            else if (it.Element("user") != null)
+            {
+                this.Id = it.Element("user", "id").ToInt();
+                this.Name = it.Element("user", "name").Value;
+            }
+        }
+    }
+    /// <summary>
+    /// XElementアクセス用の拡張メソッド
+    /// </summary>
+    internal static class XElementExtentions
+    {
+        public static XElement Element(this XElement el, string name)
+        {
+            return el.Element(XName.Get(name));
+        }
+        public static XAttribute Element(this XElement el, string name, string attr)
+        {
+            return el.Element(XName.Get(name)).Attribute(XName.Get(attr));
+        }
+        public static int ToInt(this XElement el)
+        {
+            if (string.IsNullOrEmpty(el.Value))
+                return 0;
+
+            return int.Parse(el.Value);
+        }
+        public static bool ToBool(this XElement el)
+        {
+            return bool.Parse(el.Value);
+        }
+        public static DateTime ToDatetime(this XElement el)
+        {
+            return DateTime.Parse(el.Value);
+        }
+        public static DateTime? ToDatetimeOrNull(this XElement el)
+        {
+            if (el.Value == "")
+                return null;
+            return DateTime.Parse(el.Value);
+        }
+        public static int ToInt(this XAttribute el)
+        {
+            return int.Parse(el.Value);
+        }
+
+    }
+}
---
+++ src/RmClient.Lib/RedmineService.cs
@@ -0,0 +1,296 @@
+﻿using Moonmile.Redmine.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Moonmile.Redmine
+{
+    /// <summary>
+    /// Redmine REST API 呼び出しクラス
+    /// </summary>
+    public class RedmineService
+    {
+        public ProjectService Project { get; set; }
+        public IssueService Issue { get; set; }
+        public TrackerService Tracker { get; set; }
+        public StatusService Status { get; set; }
+        public UserService User { get; set; }
+        public PriorityService Priority { get; set; }
+
+        string apikey = "6cff07e620debe042194121ea9524e60c9483880";
+        string baseurl = "http://openccpm.com/redmine/";
+        HttpClient _cl = new HttpClient();
+
+        public string ApiKey { get { return apikey; } set { apikey = value; } }
+        public string BaseUrl { get { return baseurl; } set { baseurl = value; } }
+
+        public RedmineService()
+        {
+            this.Project = new ProjectService(this);
+            this.Issue = new IssueService(this);
+            this.Tracker = new TrackerService(this);
+            this.Status = new StatusService(this);
+            this.User = new UserService(this);
+            this.Priority = new PriorityService(this);
+        }
+
+        #region // ショートカットメソッド
+        public async Task<bool> CreateObjectAsync( Issue item )
+        {
+            return await this.Issue.CreateAsync(item);
+        }
+        public async Task<bool> UpdateObjectAsync(Issue item)
+        {
+            return await this.Issue.UpdateAsync(item);
+        }
+        public async Task<List<Project>> GetProjects()
+        {
+            return await this.Project.GetListAsync();
+        }
+        public async Task<List<Issue>> GetIssues( int project_id = 0 )
+        {
+            if (project_id == 0)
+                return await this.Issue.GetListAsync();
+            else
+                return await this.Issue.GetListAsync(project_id);
+        }
+        public async Task<Project> GetProject( int id )
+        {
+            return await this.Project.GetAsync(id);
+        }
+        public async Task<Issue> GetIssue(int id)
+        {
+            return await this.Issue.GetAsync(id);
+        }
+        public async Task<List<Tracker>> GetTrackers()
+        {
+            return await this.Tracker.GetListAsync();
+        }
+        public async Task<List<Status>> GetStatuses()
+        {
+            return await this.Status.GetListAsync();
+        }
+        public async Task<List<Priority>> GetPriorities()
+        {
+            return await this.Priority.GetListAsync();
+        }
+        public async Task<List<User>> GetUsers( int project_id = 0)
+        {
+            if (project_id == 0)
+                return await this.User.GetListAsync();
+            else
+                return await this.User.GetListAsync(project_id);
+        }
+        #endregion
+
+
+        /// <summary>
+        /// 各サービスクラスのテンプレート
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        public class Service<T> where T : new() 
+        {
+            protected HttpClient _cl;
+            protected string _apikey;
+            protected string _baseurl;
+            protected string _tablename;
+
+            public Service(RedmineService rs, string tablename )
+            {
+                _cl = rs._cl;
+                _baseurl = rs.baseurl;
+                _apikey = rs.apikey;
+                _tablename = tablename;
+            }
+
+            /// <summary>
+            /// new T(el) の代わりに、CreateItem を用意してオーバーライドさせる
+            /// </summary>
+            /// <param name="el"></param>
+            /// <returns></returns>
+            internal Func<XElement, T> CreateItem = (el) => { return new T(); };
+
+            /// <summary>
+            /// 全体リストを取得
+            /// </summary>
+            /// <returns></returns>
+            public virtual async Task<List<T>> GetListAsync()
+            {
+                var url = $"{_baseurl}{_tablename}.xml?key={_apikey}";
+                var xml = await _cl.GetStringAsync(url);
+                var doc = XDocument.Load(new System.IO.StringReader(xml));
+                var items = new List<T>();
+                foreach (var it in doc.Root.Elements())
+                {
+                    items.Add(CreateItem(it));
+                }
+                return items;
+            }
+
+            /// <summary>
+            /// 親IDを指定してリストを取得
+            /// </summary>
+            /// <param name="pname"></param>
+            /// <param name="pid"></param>
+            /// <returns></returns>
+            public async Task<List<T>> GetListAsync(string pname, int pid)
+            {
+                var url = $"{_baseurl}{_tablename}.xml?{pname}={pid}&key={_apikey}";
+                var xml = await _cl.GetStringAsync(url);
+                var doc = XDocument.Load(new System.IO.StringReader(xml));
+                var items = new List<T>();
+                foreach (var it in doc.Root.Elements())
+                {
+                    items.Add(CreateItem(it));
+                }
+                return items;
+            }
+            /// <summary>
+            /// IDを指定して単独のアイテムを取得
+            /// </summary>
+            /// <param name="id"></param>
+            /// <returns></returns>
+            public virtual async Task<T> GetAsync( int id)
+            {
+                var url = $"{_baseurl}{_tablename}/{id}.xml?key={_apikey}";
+                var xml = await _cl.GetStringAsync(url);
+                var doc = XDocument.Load(new System.IO.StringReader(xml));
+                var item = CreateItem(doc.Root);
+                return item;
+            }
+            /*
+            public virtual async void CreateAsync(T item)
+            {
+
+            }
+            public virtual async void UpdateAsync(T item)
+            {
+
+            }
+            public virtual async void RemoveAsync(int id)
+            {
+
+            }
+            */
+        }
+
+        /// <summary>
+        /// プロジェクト情報へアクセス
+        /// </summary>
+        public class ProjectService : Service<Project>
+        {
+            public ProjectService(RedmineService rs) : base( rs, "projects" )
+            {
+                this.CreateItem = el => new Model.Project(el);
+            }
+        }
+        /// <summary>
+        /// チケット情報へのアクセス
+        /// </summary>
+        public class IssueService : Service<Issue>
+        {
+            public IssueService(RedmineService rs) : base( rs, "issues" )
+            {
+                this.CreateItem = el => new Model.Issue(el);
+            }
+            /// <summary>
+            /// プロジェクトIDを指定して、チケット一覧を取得
+            /// </summary>
+            /// <param name="pid"></param>
+            /// <returns></returns>
+            public Task<List<Issue>> GetListAsync( int pid )
+            {
+                return base.GetListAsync("project_id", pid);
+            }
+            /// <summary>
+            /// 既存チケットを更新
+            /// </summary>
+            /// <param name="item"></param>
+            public async Task<bool> UpdateAsync(Issue item)
+            {
+                var url = $"{_baseurl}{_tablename}/{item.Id}.xml?key={_apikey}";
+                var xml = item.ToXml();
+                var contnet = new StringContent(xml, Encoding.UTF8, "application/xml");
+                var res = await _cl.PutAsync(url, contnet);
+                return true;
+            }
+            /// <summary>
+            /// 新規チケットを更新
+            /// </summary>
+            /// <param name="item"></param>
+            public async Task<bool> CreateAsync(Issue item)
+            {
+                var url = $"{_baseurl}{_tablename}.xml?key={_apikey}";
+                var xml = item.ToXml();
+                var contnet = new StringContent(xml, Encoding.UTF8, "application/xml");
+                var res = await _cl.PostAsync(url, contnet);
+                return true;
+            }
+        }
+        /// <summary>
+        /// トラッカー一覧へのアクセス
+        /// </summary>
+        public class TrackerService : Service<Tracker>
+        {
+            public TrackerService(RedmineService rs) : base(rs, "trackers")
+            {
+                this.CreateItem = el => new Model.Tracker(el);
+            }
+        }
+        /// <summary>
+        /// ステータス一覧へのアクセス
+        /// </summary>
+        public class StatusService : Service<Status>
+        {
+            public StatusService(RedmineService rs) 
+                : base( rs, "issue_statuses")
+            { 
+                this.CreateItem = el => new Model.Status(el);
+            }
+        }
+        /// <summary>
+        /// 優先度一覧へのアクセス
+        /// </summary>
+        public class PriorityService : Service<Priority>
+        {
+            public PriorityService(RedmineService rs)
+                : base(rs, "enumerations/issue_priorities")
+            {
+                this.CreateItem = el => new Model.Priority(el);
+            }
+        }
+        /// <summary>
+        /// ユーザー一覧へのアクセス
+        /// </summary>
+        public class UserService : Service<User>
+        {
+            public UserService(RedmineService rs)
+                : base(rs, "users")
+            {
+                this.CreateItem = el => new Model.User(el);
+            }
+            /// <summary>
+            /// プロジェクトIDを指定してリストを取得
+            /// </summary>
+            /// <param name="pname"></param>
+            /// <param name="pid"></param>
+            /// <returns></returns>
+            public async Task<List<User>> GetListAsync(int pid)
+            {
+                var url = $"{_baseurl}projects/{pid}/memberships.xml?key={_apikey}";
+                var xml = await _cl.GetStringAsync(url);
+                var doc = XDocument.Load(new System.IO.StringReader(xml));
+                var items = new List<User>();
+                foreach (var it in doc.Root.Elements())
+                {
+                    items.Add(new User(it));
+                }
+                return items;
+            }
+        }
+    }
+}
